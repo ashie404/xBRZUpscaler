8698ddb860e12b688a167b9c58c93658e7f4da68
June <22226164+juniebyte@users.noreply.github.com>
2022-09-19 13:46:15 -0500

    fix

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -4,6 +4,6 @@
 #define UPSCALE_HAND // Whether to upscale your hand or it's items or not.
 #define UPSCALE_PARTICLES // Whether to upscale particles or not. Does not affect weather.
 #define UPSCALE_BEACONBEAM // Whether to upscale the beacon beam or not.
-//#define UPSCALE_SCREEN // Upscales game screen. Hits performance pretty bad. Works ok as an anti-aliasing filter.
+//#define UPSCALE_SCREEN // Upscales game screen. Hits performance pretty bad. Works "ok" as an anti-aliasing filter.
 
-#define INFO 0 // xBRZUpscale shader by J0SH. [0 1]
+#define INFO 0 // xBRZUpscale shader by Ashie! [0 1]
